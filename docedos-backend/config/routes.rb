98f73a0df7b453338e6fb2eb6b3aaefdf9f31ebb
Vicente Meza <vmeza3501@gmail.com>
2026-05-18 18:58:14 -0400

    Fix: habilitar endpoint /up para health check

--- docedos-backend/config/routes.rb
+++ docedos-backend/config/routes.rb
@@ -1,5 +1,8 @@
 # config/routes.rb
 Rails.application.routes.draw do
+  # Endpoint para verificar la salud de la app (vuelve a activar la URL /up)
+  get "up" => "rails/health#show", as: :rails_health_check
+
   post '/register', to: 'auth#register'
   post '/login', to: 'auth#login'
 end
