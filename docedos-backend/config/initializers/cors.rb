4853a1ce443f0fb0a8e551569ce59c1e1cf78860
Vicente Meza <vmeza3501@gmail.com>
2026-05-18 18:33:29 -0400

    Fix: cors origin

--- docedos-backend/config/initializers/cors.rb
+++ docedos-backend/config/initializers/cors.rb
@@ -1,7 +1,8 @@
-# config/initializers/cors.rb
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://docedos-production.up.railway.app', 'https://docedos-production.up.railway.app/login', 'http://localhost:3000' # En producción, cambia "*" por el dominio de tu frontend
+    # Deja únicamente los dominios base sin rutas (/login) ni barras diagonales finales (/)
+    origins 'https://docedos-production.up.railway.app', 'http://localhost:3000'
+
     resource "*",
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
